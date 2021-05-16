3c7ee063ed9843bd459958b6309ef0b9e5513ea0
Manhcuong12345 <62935762+Manhcuong12345@users.noreply.github.com>
2021-05-16 17:26:01 +0700

    bug

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express')
 var bodyParser = require('body-parser')
 const db = require('./db')
-const cors = require('cors')
+// const cors = require('cors')
 const routes = require('./start_up/routes')
 const app = express()
 
-app.use(cors())
+// app.use(cors())
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 db.connect()
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
 
 routes(app)
 
