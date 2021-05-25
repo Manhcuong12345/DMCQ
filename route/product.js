b395b36807bedc9d9c314eff3f52655cc8684dfd
Manhcuong12345 <62935762+Manhcuong12345@users.noreply.github.com>
2021-05-25 20:24:42 +0700

    bug

--- route/product.js
+++ route/product.js
@@ -6,7 +6,7 @@ const auth = require('../middleware/verifyToken')
 const { adminMiddleware } = require('../middleware/roleMiddlware')
 
 router.get('/', Productcontroller.getAll)
-router.get('/:id', auth, Productcontroller.detailProduct)
+router.get('/:id', Productcontroller.detailProduct)
 // router.get('/:slug',auth,Productcontroller.filterPrice)
 router.post('/', auth, adminMiddleware, upload.single("product_img"), Productcontroller.createProduct)
 router.delete('/:id', auth, adminMiddleware, Productcontroller.deleteProduct)
